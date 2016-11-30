6f0ec5352d400cbfdcadf137c5a85cf0d4d1a9cc
Chet <chet@reldo.xyz>
2016-12-01 00:24:19 +0100

    quickie

--- basic_example.php
+++ basic_example.php
@@ -1,11 +1,9 @@
 <?php
 require_once 'rue.php';
-require_once 'rollingCurl.php';
 
 $r = new Rue();
 
-$list = $r->get_clan_list_light("Wrack City");
-$demo = $r->get_multi_activity($list);
+$demo = $r->get_player_skills("Chet Faker");
 
 echo "<pre>";
 var_dump($demo);
